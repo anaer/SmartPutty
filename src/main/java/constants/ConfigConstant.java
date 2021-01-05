748021131dfc7486c63c64f6e1139b9ce77b5dd3
lvcn <13819193282@139.com>
2021-01-05 20:54:31 +0800

    调整常量顺序

--- src/main/java/constants/ConfigConstant.java
+++ src/main/java/constants/ConfigConstant.java
@@ -4,6 +4,18 @@ public class ConfigConstant {
     private ConfigConstant() {
     }
 
+    /**
+     * 配置组
+     */
+    public static final String GROUP_CONFIGURATION    = "configuration";
+
+    public static final String GROUP_FEATURE          = "feature";
+
+    public static final String GROUP_PROGRAM          = "program";
+
+    /**
+     * configuration配置项.
+     */
     public static final String WAIT_FOR_INIT_TIME     = "WaitForInitTime";
 
     public static final String VIEW_UTILITIES_BAR     = "ViewUtilitiesBar";
@@ -11,12 +23,6 @@ public class ConfigConstant {
     public static final String VIEW_CONNECTION_BAR    = "ViewConnectionBar";
 
     public static final String VIEW_BOTTOM_QUICK_BAR  = "ViewBottomQuickBar";
-
-    public static final String GROUP_CONFIGURATION    = "configuration";
-
-    public static final String GROUP_FEATURE          = "feature";
-
-    public static final String GROUP_PROGRAM          = "program";
 
     public static final String VERSION                = "Version";
 
@@ -26,18 +32,17 @@ public class ConfigConstant {
 
     public static final String DATABASE_PATH          = "DatabasePath";
 
-    /**
-    *
-    */
     public static final String WINDOWS_BASE_DRIVE     = "WindowsBaseDrive";
 
-    /**
-    *
-    */
     public static final String SHOW_WELCOME_PAGE      = "ShowWelcomePage";
 
-    /**
-    *
-    */
     public static final String WINDOW_POSITION_SIZE   = "WindowPositionSize";
+
+    /**
+     * feature配置项.
+     */
+
+    /**
+     * program配置项.
+     */
 }
